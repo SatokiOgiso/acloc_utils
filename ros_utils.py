d82bc75fd683cf5e7a5acc2bd99eb8450d8e7bda
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2018-05-08 20:31:08 +0900

    initial commit

---
+++ ros_utils.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+import tf
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, PoseArray
+
+
+def euler_to_quaternion(euler):
+    """Convert Euler Angles to Quaternion
+
+    euler: geometry_msgs/Vector3
+    quaternion: geometry_msgs/Quaternion
+    """
+    q = tf.transformations.quaternion_from_euler(euler[0], euler[1], euler[2])
+    return Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+
+
+def get_param_if_exists(param_name, default_val):
+    """ get a param from the parameter server if the specified parameter exists.
+        if not, the default value will be returned
+
+    :param param_name: parameter name to be loaded. String
+    :param default_val: default value for the parameter.
+    :return: gotten parameter if the parameter exists, default_val if not.
+    """
+
+    if rospy.has_param(param_name):
+        return rospy.get_param(param_name)
+    else:
+        rospy.logwarn(param_name + " was not specified at parameter. Using default.")
+        return default_val
+
+
+def pack_location_PoseWithCovarianceStamped(localizer, stamp=None, frame_id="/world"):
+    location = PoseWithCovarianceStamped()
+    if stamp is None:
+        stamp = rospy.Time.now()
+    location.header.stamp = stamp
+    location.header.frame_id = frame_id
+    location.pose.pose.position.x = localizer.x[0].copy()
+    location.pose.pose.position.y = localizer.x[1].copy()
+    location.pose.pose.orientation = euler_to_quaternion([0, 0, localizer.x[2].copy()])
+    if hasattr(localizer, "P"):
+        location.pose.covariance = convertCovariance2Dto3D(localizer.P).flatten("F")
+
+    return location
+
+
+def pack_location_PoseArray(particles, stamp=None, frame_id="/world"):
+    location_array = PoseArray()
+    if stamp is None:
+        stamp = rospy.Time.now()
+    location_array.header.stamp = stamp
+    location_array.header.frame_id = frame_id
+    for particle in particles:
+        p = Pose()
+        p.position.x = particle.pos[0].copy()
+        p.position.y = particle.pos[1].copy()
+        p.orientation = euler_to_quaternion([0, 0, particle.pos[2].copy()])
+        location_array.poses.append(p)
+
+    return location_array
+
+
+def convertCovariance2Dto3D(covariance2d):
+    """ convert the covariance from [x, y, theta] to [x, y, z, roll, pitch, yaw]
+
+    :param covariance2d: covariance matrix in 3x3 format. each row and column corresponds to [x, y, theta]
+    :return: covariance matrix in 6x6 format. each row and column corresponds to [x, y, z, roll, pitch, yaw], where z, roll and pitch values are padded with 0.
+    """
+
+    covariance3d = np.zeros([6, 6])
+    covariance2d = np.array(covariance2d)
+    covariance3d[0:1, 0:1] = covariance2d[0:1, 0:1]
+    covariance3d[5, 0:1] = covariance2d[2, 0:1]
+    covariance3d[0:1, 5] = covariance2d[0:1, 2]
+    covariance3d[5, 5] = covariance2d[2, 2]
+
+    return covariance3d
+
+
+def scale_data(data, maxval=100):
+    """ scale the data into 0 to maxval
+
+    :param data: data to be scaled.
+    :param maxval: maximum value of the scaling range
+    :return: scaled data
+    """
+
+    max_data = np.max(data)
+    min_data = np.min(data)
+    scale = max_data - min_data
+
+    image_sc = maxval * (data - min_data) / scale
+
+    return image_sc.astype("uint8")
